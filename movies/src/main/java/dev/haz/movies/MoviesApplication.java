88077d3b6ff7732d581bafabb9860e8a762db8be
cloud9ight <cloud9ight@gmail.com>
2024-06-29 17:57:11 +0800

    adding java files using spring boot templates

---
+++ movies/src/main/java/dev/haz/movies/MoviesApplication.java
@@ -0,0 +1,20 @@
+package dev.haz.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+//	@GetMapping("/")
+//	public String apiRoot() {
+//		return "Hello World!";
+//	}
+
+}
