c9096571952723e3bcefbb0ed6ff00754922434d
cloud9ight <cloud9ight@gmail.com>
2024-07-04 21:46:04 +0800

    add findMovieByImdbId

--- movies/src/main/java/dev/haz/movies/MovieService.java
+++ movies/src/main/java/dev/haz/movies/MovieService.java
@@ -1,9 +1,14 @@
 package dev.haz.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,5 +16,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
